0904f95a69682376884509f86e392b20e60208cf
Amerson <amersonferrer@gmail.com>
2021-12-18 21:06:10 +0000

    Base
    The fundamentals codes are finished, changes colour of the background

---
+++ script.js
@@ -0,0 +1,23 @@
+function colourChange() {
+    const number1 = Math.floor(Math.random() * 255) ;
+    const number2 = Math.floor(Math.random() * 255) ;
+    const number3 = Math.floor(Math.random() * 255) ;
+    
+    rgbString = `rgb(${number1}, ${number2}, ${number3})`
+    
+    document.body.style.background = rgbString; // gets the HTML Background and changes the colour based from rgbString
+
+    document.getElementById("rgbNumber").innerHTML = rgbString; //prints the rgb number underneath
+    
+    
+    console.log(rgbString); // to see if the function works
+}
+
+
+
+// "OPTIMAL" way 
+
+//This function prevents doing certain methods multipl times
+function colourNumber(){
+    return Math.floor(Math.random() * 255)
+} 
